4934cc4f7ba4eec9d04365ea9d42e54cbdee4158
Amar <149885792+amarsalc@users.noreply.github.com>
2024-04-26 11:27:38 +0200

    Update Jump_Sturdy_Board_Class.py
    Methode zum initialisieren der BitBoards aus dem gegeben FEN String hinzugefügt.

--- code/Jump_Sturdy_Board_Class.py
+++ code/Jump_Sturdy_Board_Class.py
@@ -1,4 +1,11 @@
 class JumpSturdyBoard:
+    #Array mit allen Bit Boards der Figuren aus Jump-Sturdy bestehen
+    pieceBB = [0b0, 0b0, 0b0, 0b0, 0b0, 0b0]
+    #Repräsentation von leeren Feldern in einem BB
+    emptyBB=0b0
+    #Repräsentation von besetzten Feldern in einem BB
+    occupiedBB=0b0
+    
     def __init__(self):
         # initialisiere leeres board
         # in dem fall noch ein 8x8 board
@@ -11,7 +18,55 @@ class JumpSturdyBoard:
     def turn_counter(self):
         # zähle die Anzahl an Zügen
         self.turn_counter += 1
-        
+
+    def fen_notation_into_bb(notation):
+        #Auslesen der vorgegebenen String Notation vom Server in unsere BitBoards für Pieces
+        binary = ""
+        boardPos = 59
+        i = 0
+        #Gehe die String Notation bis zum Schluss durch.
+        while i < len(notation):
+            #Setze den String in eine Binäre Darstellung und befülle ihn 60 mal mit der "0", weil unsere BitBoards nur 60 Felder haben.
+            binary = "0"*60
+            #Teile den String an der Board Position in der wir uns gerade gefinden und setze eine 1 dazwischen. 
+            binary = binary[boardPos+1:] + "1" + binary[0:boardPos]
+            #Schaue ob das Zeichen aus der Notation ein "r" ist.
+            if notation[i] == "r":
+                #Schaue, ob das nächste Zeichen aus der Notation ebenfalls ein "r" ist, denn daraus Ergibt sich ein Pferd.
+                if notation[i] == notation[i + 1]:
+                    #Wandle den String in ein binäres int um und addiere es auf das aktuelle BitBoard für rote Pferde. 
+                    PBoard.pieceBB[2]+=int(binary,2)
+                #Schaue, ob das nächste Zeichen aus der Notation ein "b" ist, denn daraus Ergibt sich ein captured red pawn(Mir fällt der deutsche Name nicht ein).
+                elif notation[i+1] == "b":
+                    #Wandle den String in ein binäres int um und addiere es auf das aktuelle BitBoard für captured red pawn.
+                    PBoard.pieceBB[4] += int(binary, 2)
+                else:
+                    #Wandle den String in ein binäres int um und addiere es auf das aktuelle BitBoard für rote Bauern.
+                    PBoard.pieceBB[0] += int(binary, 2)
+            #Falls das Zeichen ein "/" soll dieser Schleifendurchlauf überprüfen werden, da es kein Zeichen ist, welches im Board notwendig ist.
+            elif notation[i] == "/":
+                i += 1
+                continue
+            #Falls das Zeichen aus der Notation ein "b" ist.
+            elif notation[i] == "b":
+                if notation[i] == notation[i + 1]:
+                    #Wandle den String in ein binäres int um und addiere es auf das aktuelle BitBoard für blaue Pferde. 
+                    PBoard.pieceBB[3] += int(binary, 2)
+                elif notation[i+1] == "r":
+                    #Wandle den String in ein binäres int um und addiere es auf das aktuelle BitBoard für captured blue pawn.
+                    PBoard.pieceBB[5] += int(binary, 2)
+                else:
+                    #Wandle den String in ein binäres int um und addiere es auf das aktuelle BitBoard für blaue Bauern.
+                    PBoard.pieceBB[1] += int(binary, 2)
+            #Falls das Zeichen aus der Notation eine Zahl ist.
+            else:
+                #Reduziere die Board Position um die angegebene Zahl für die leeren Felder. 
+                boardPos -= int(notation[i])
+                i += 1
+                continue
+            #Reduziere die Board Position um 1 und erhöhe die nächste Iteration für die Schleife um 2.
+            boardPos -= 1
+            i += 2
     
     def put_pieces_on_board(self):
         # setze die Steine auf das Board
